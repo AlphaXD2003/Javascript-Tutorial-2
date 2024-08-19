d6ecbd2cd062c86bd8377b223feaa3e832de0dd1
AlphaXD2003 <alphaxd2023@gmail.com>
2024-08-19 10:29:25 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+{
+    var a = 'wribhu';
+
+    {
+        var a = 'sayani'
+        console.log(a)
+    }
+    console.log(a)
+    
+}
+
+// let : scope specific
+// var : global
+
+// console.log(a)
