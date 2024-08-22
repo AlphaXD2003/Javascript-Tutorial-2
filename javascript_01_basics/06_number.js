d5099d32a060243de12dfc09ed25b54cdd9be666
AlphaXD2003 <alphaxd2023@gmail.com>
2024-08-22 10:40:48 +0530

    number and math are done

---
+++ javascript_01_basics/06_number.js
@@ -0,0 +1,56 @@
+const score = 10203098697
+const phonenumber = 916290987678
+// const score1 =new Number(10 )
+console.log(phonenumber[0])
+console.log(phonenumber[1])
+console.log(String(phonenumber).slice(0,2))
+console.log(phonenumber.toString())
+
+console.log(
+    phonenumber
+    .toString()
+    .slice(0,2)
+)
+console.log(
+    phonenumber
+    .toString() [0]
+   
+)
+
+// '916290987678'[0]
+
+// 3.14
+
+
+console.log(typeof parseFloat(((22 / 7).toFixed(2))))
+// (3.142857142857143).toFixed(2)
+// => 3.14
+// => '3.14'
+
+// '3'
+// console.log(parseInt('3', 2))
+console.log(parseInt('11', 10))
+console.log(parseInt('11', 2))
+// console.log(parseInt('0x11', 16))
+
+
+const anotherNumber = 1234567890352467
+console.log(anotherNumber.toFixed(2))
+console.log(anotherNumber.toPrecision(5))
+
+
+// 1e15 => (10 x 1) ^ 15
+// 1.2e15 => (1.2 x 10) ^ 
+// 1.2 x 10^15
+// 12 x 100000000000000 
+// 1234500000000000 
+// 1234567890352467
+
+
+
+
+const paymentAmount = 1234567890352467
+console.log(paymentAmount.toLocaleString('en-US'))
+
+// 1,23,45,67,89,03,52,467
+// 1,234,567,890,352,467
---
+++ javascript_01_basics/07_math.js
@@ -0,0 +1,53 @@
+console.log(Math.ceil(10.5))
+console.log(Math.floor(10.5))
+console.log(Math.round(10.5))
+console.log(Math.abs(-10))
+
+console.log(Math.min(10, 20 , 13,2))
+console.log(Math.max(10, 20 , 13,2))
+
+
+console.log(Math.random())
+
+
+// return random value in between 10 and 11
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.random() * (11 - 10) + 10)
+// 0.025617449488421595
+// 0.7042611724862633
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
