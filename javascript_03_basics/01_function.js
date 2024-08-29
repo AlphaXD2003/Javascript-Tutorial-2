568132f2fc9c7ee0e9d84763311fa6f3604d5b25
AlphaXD2003 <alphaxd2023@gmail.com>
2024-08-29 10:50:05 +0530

    chore: Add function examples to JavaScript basics

---
+++ javascript_03_basics/01_function.js
@@ -0,0 +1,45 @@
+function printSomething() {
+  let a = 20;
+  let b = 30;
+  let c = a * b;
+  return c;
+  //   return a*b;
+  console.log("hi");
+}
+
+let result = printSomething();
+// console.log(result);
+
+function multiplyTwo(a = 21, b = 22) {
+  // let a = 24
+  //let b = 20
+  // b = 32
+  // 24 * 20
+  return a * b;
+}
+
+// console.log(multiplyTwo(20, 30));
+// console.log(multiplyTwo(22, 32));
+// console.log(multiplyTwo(24));
+// console.log(multiplyTwo(22, 32));
+// console.log(multiplyTwo(undefined, 23));
+
+function sum(a, ...c) {
+  //   console.log(c); [4,5]
+  console.log(...c); // 4 5
+  let sum = a;
+  c.forEach((val) => {
+    sum += val;
+  });
+  console.log(sum);
+  //   console.log(typeof c);
+  //   return a + b;
+}
+
+// console.log(sum(2, 3));
+// console.log(sum(3, 3));
+// console.log(sum(2, 3, 4, 5));
+// console.log(sum(2, 3, 4, 5));
+sum(2, 3, 4, 5);
+sum(2, 3, 4, 5, 6);
+sum(2, 3, 4, 5, 6, 7);
