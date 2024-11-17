50e8238beac6b1e8775fcdef73349aaa2bc0a5b4
AlphaXD2003 <alphaxd2023@gmail.com>
2024-11-17 10:22:29 +0530

    tdz

---
+++ javascript_07_advance/03_prototype.js
@@ -0,0 +1,9 @@
+function donothing(){
+    console.log(`Eat 5 star and do nothing`)
+}
+
+// donothing()
+
+donothing.name1 = 'ayandip'
+console.log(donothing.name1)
+console.log(donothing.prototype)
---
+++ javascript_07_advance/04_tdz.js
@@ -0,0 +1,16 @@
+console.log(x)
+var x = 10
+console.log(x)
+sayHello()
+// function sayHello(){
+//     console.log("Hello")
+// }
+
+// var sayHello = function (){
+//     console.log("Hello")
+// }
+
+var sayHello = () => {
+    console.log('Hello')
+}
+
